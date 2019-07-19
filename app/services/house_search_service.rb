54925aa1ed98642f2e4f882714fd96ee1069cb5e
davisc0801 <davisc1014@gmail.com>
2019-07-19 10:30:35 -0600

    Add new API handshake

--- app/services/house_search_service.rb
+++ app/services/house_search_service.rb
@@ -1,14 +1,29 @@
 class HouseSearchService
   def initialize(house)
-    @house = house.downcase
+    @house = format(house.downcase.to_sym)
   end
 
   def connection
-    Faraday.new("https://westerosapi.herokuapp.com")
+    Faraday.new("https://westeros-as-a-service.herokuapp.com") do |faraday|
+      faraday.headers['x_api_key'] = ENV["WESTEROS_API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  #hardcoding to avoid a second API hit, more extensable to check the API for houses
+  def format(house)
+    house_ids = {
+      stark: 1,
+      lannister: 2,
+      targaryen: 3,
+      tyrell: 4,
+      greyjoy: 5,
+    }
+    house_ids[house]
   end
 
   def search
-    response = connection.get("/api/v1/house/#{@house}", api_key: ENV["WESTEROS_API_KEY"])
+    response = connection.get("/api/v1/house/#{@house}")
     data = JSON.parse(response.body, symbolize_names: true)
   end
 end
